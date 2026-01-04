a07b98f9db834de8376363a18c9d86e935c89ef1
Paul Velastegui <fpaulvelastegui@gmail.com>
2026-01-04 11:32:10 -0800

    accesos directos clic en mapa y nueva fuentes

--- src/components/Ubicacion.jsx
+++ src/components/Ubicacion.jsx
@@ -28,7 +28,9 @@ export default function Ubicacion() {
                             </div>
                             <div className="ubicacion__map-col">
                                 <div className="ubicacion__mini-map">
-                                    <img src={miniMap} alt="Mapa Quito" />
+                                    <a href="https://www.google.com/maps/place/Axxis+Centro+%C3%93ptico/@-0.1703729,-78.4874886,3a,75y,256.63h,76.52t/data=!3m10!1e1!3m8!1sLhkryFPoAYxh4T2qGU1DtA!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fcb_client%3Dmaps_sv.tactile%26w%3D900%26h%3D600%26pitch%3D13.482803743301972%26panoid%3DLhkryFPoAYxh4T2qGU1DtA%26yaw%3D256.6317895718909!7i16384!8i8192!9m2!1b1!2i45!4m15!1m8!3m7!1s0x91d59a9096746a83:0x57ae48ab4cd38ef!2sAv.+10+de+Agosto+39-155,+Quito+170104,+Ecuador!3b1!8m2!3d-0.170664!4d-78.487609!16s%2Fg%2F11yjz_sm2k!3m5!1s0x91d59b806960279d:0xe5ef54947e7a20d8!8m2!3d-0.1702241!4d-78.4875234!16s%2Fg%2F11n83gt64t?entry=ttu&g_ep=EgoyMDI1MTIwOS4wIKXMDSoASAFQAw%3D%3D" target="_blank" rel="noreferrer">
+                                        <img src={miniMap} alt="Mapa Quito" />
+                                    </a>
                                 </div>
                                 <div className="ubicacion__s-circle">S</div>
                             </div>
@@ -45,7 +47,9 @@ export default function Ubicacion() {
                             </div>
                             <div className="ubicacion__map-col">
                                 <div className="ubicacion__mini-map">
-                                    <img src={miniMap} alt="Mapa Ambato" />
+                                    <a href="https://www.google.com/maps/place/Darquea+%26+5+de+Junio,+Ambato,+Ecuador/@-1.2359672,-78.6252346,17z/data=!3m1!4b1!4m6!3m5!1s0x91d381a552386e51:0x8d579d3870e3004a!8m2!3d-1.2359726!4d-78.6226597!16s%2Fg%2F11gf21j3sg?entry=ttu&g_ep=EgoyMDI1MTIwOS4wIKXMDSoASAFQAw%3D%3D" target="_blank" rel="noreferrer">
+                                        <img src={miniMap} alt="Mapa Ambato" />
+                                    </a>
                                 </div>
                                 <div className="ubicacion__s-circle">S</div>
                             </div>
